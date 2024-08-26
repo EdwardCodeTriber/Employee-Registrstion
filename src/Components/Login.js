ed81e51d91a1bcad82cc8d2b1c9ced009ed4d8b9
EdwardThapelo <edwardthapelo55@gmail.com>
2024-08-26 20:42:50 +0200

    Fixed the Login Local Storage code

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,45 +1,66 @@
-import React, { useState } from 'react'
-import {useNavigate} from 'react-router-dom'
-import './Login.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
+  const navigate = useNavigate();
+  //default admin login credentials
+//   const userName = localStorage.setItem("email")
+//     ? localStorage.getItem("email")
+//     : "admin@admin1.com";
+//   const userPassword = localStorage.setItem("password")
+//     ? localStorage.getItem("password")
+//     : "admin1";
+  //
 
-    const navigate = useNavigate();
-    //default admin login credentials
-    const userName = localStorage.getItem('email') ? localStorage.getItem('email') : 'admin@admin1.com'
-    const userPassword = localStorage.getItem('password') ? localStorage.getItem('password') : 'admin1'
-    //
-    const handleSubmit= (e) => {
-        e.preventDefault();
-        if(email === userName && password === userPassword){
-            // alert("Correct Credentials")
-            navigate('/Home')
-        }else{
-            alert=("Incorrect details");
-        }
+  // Store email and password
+  localStorage.setItem("email", "admin@admin1.com");
+  localStorage.setItem("password", "admin1");
+
+  // Retrieve email and password
+  const userName = localStorage.getItem("email") || "admin@admin1.com";
+  const userPassword = localStorage.getItem("password") || "admin1";
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email === userName && password === userPassword) {
+      alert("Correct Credentials");
+      navigate("/Home");
+    } else {
+      alert = "Incorrect details";
     }
-
+  };
 
   return (
     <div>
-        
-        <div className='form-container'>
-            <form className='form-list'>
-            <label>UserName</label>
-                <input type='email' required className='form-item' 
-                value={email} onChange={e => setEmail(e.target.value)}></input>
-                <br></br>
-                <label>Password</label>
-                <input type='text' required className='form-item' 
-                value={password} onChange={e => setPassword(e.target.value)}></input>
-            </form>
-            <button type='submit' className='login-btn' onClick={handleSubmit}>Login</button>
-        </div>
+      <div className="form-container">
+        <form className="form-list">
+          <label>UserName</label>
+          <input
+            type="email"
+            required
+            className="form-item"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <br></br>
+          <label>Password</label>
+          <input
+            type="text"
+            required
+            className="form-item"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </form>
+        <button type="submit" className="login-btn" onClick={handleSubmit}>
+          Login
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import Navbar from '../Components/Navbar';
-import '../Components/Register.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+import "../Components/Register.css";
 
 const Registration = () => {
   const navigate = useNavigate();
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [number, setNumber] = useState('');
-  const [image, setImage] = useState('');
-  const [position, setPosition] = useState('');
-  const [id, setID] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [image, setImage] = useState("");
+  const [position, setPosition] = useState("");
+  const [id, setID] = useState("");
 
   const [employees, setEmployees] = useState([]);
 
   useEffect(() => {
-    const storedEmployees = JSON.parse(localStorage.getItem('employees')) || [];
+    const storedEmployees = JSON.parse(localStorage.getItem("employees")) || [];
     setEmployees(storedEmployees);
   }, []);
 
@@ -43,9 +43,9 @@ const Registration = () => {
       };
       const updatedEmployees = [...employees, newEmployee];
       setEmployees(updatedEmployees);
-      localStorage.setItem('employees', JSON.stringify(updatedEmployees));
+      localStorage.setItem("employees", JSON.stringify(updatedEmployees));
       alert("Added Successfully");
-      navigate('/Home');
+      navigate("/Home");
     }
   };
 
@@ -53,52 +53,77 @@ const Registration = () => {
     <div>
       <Navbar />
       <h1>Employee Registrations</h1>
-      <div className='form-container'>
-        <form autoComplete='off' className='form-list' onSubmit={handleSubmit}>
+      <div className="form-container">
+        <form autoComplete="off" className="form-list" onSubmit={handleSubmit}>
           <label>Name</label>
-          <input 
-            type='text' required className='form-item' placeholder='Name...'
-            value={name} onChange={e => setName(e.target.value)}
+          <input
+            type="text"
+            required
+            className="form-item"
+            placeholder="Name..."
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <br />
           <label>Email</label>
-          <input 
-            type='email' required className='form-item' placeholder='Email...'
-            value={email} onChange={e => setEmail(e.target.value)}
+          <input
+            type="email"
+            required
+            className="form-item"
+            placeholder="Email..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <br />
           <label>Phone Number</label>
-          <input 
-            type='number' required className='form-item' placeholder='Phone Number...'
-            value={number} onChange={e => setNumber(e.target.value)}
+          <input
+            type="number"
+            required
+            className="form-item"
+            placeholder="Phone Number..."
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
           />
           <br />
           <label>Image</label>
-          <input 
-            type='file' id='upload' accept='image/*' required className='form-item' placeholder='Image...'
-            onChange={e => setImage(e.target.files[0])}
+          <input
+            type="file"
+            id="upload"
+            accept="image/*"
+            required
+            className="form-item"
+            placeholder="Image..."
+            onChange={(e) => setImage(e.target.files[0])}
           />
           <br />
           <label>Position</label>
-          <input 
-            type='text' required className='form-item' placeholder='Position...'
-            value={position} onChange={e => setPosition(e.target.value)}
+          <input
+            type="text"
+            required
+            className="form-item"
+            placeholder="Position..."
+            value={position}
+            onChange={(e) => setPosition(e.target.value)}
           />
           <br />
           <label>ID</label>
-          <input 
-            type='text' required className='form-item'
-            value={id} onChange={e => setID(e.target.value)}
+          <input
+            type="text"
+            required
+            className="form-item"
+            value={id}
+            onChange={(e) => setID(e.target.value)}
           />
           <br />
-          <button type='submit' className='add-btn'>Register Employee</button>
+          <button type="submit" className="add-btn">
+            Register Employee
+          </button>
           <br />
           <Link to="/Home">Cancel</Link>
         </form>
       </div>
-      
     </div>
   );
-}
+};
 
 export default Registration;
